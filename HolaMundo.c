e07235cac804b80c735af9e837cd4672c990c70f
Belen Leon <bplr1@alu.ua.es>
2021-12-29 19:48:37 +0100

    Arreglo código para Helloworld simple

--- HolaMundo.c
+++ HolaMundo.c
@@ -2,18 +2,8 @@
 #include <stdlib.h>
 
 
-#include <libintl.h>
-#include <locale.h>
-
-#define _(STRING) gettext(STRING)
-
 
 int main() {
-
-	setlocale (LC_ALL, "");		//prepara la app para el locale
-	bindtextdomain ("HolaMundo", "po");	//"HolaMundo" nombre de proyecto 
-	textdomain ("HolaMundo");
-	//printf("Curr. dir: %s\n", getenv("pwd"));
 
 	//uso 11n
 	printf(_("Hola mundo. Soy Belen\n"));
